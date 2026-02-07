9651a1020efed1f43b6cd35318b76a5cbef72dd1
Stan Vlad <vstan02@protonmail.com>
2026-02-07 22:42:15 +0200

    Sync.

---
+++ lib/lexer.mli
@@ -0,0 +1,21 @@
+(* Copyright (c) 2025 Stan Vlad <vstan02@protonmail.com>
+ *
+ * This file is part of Thalia.
+ *
+ * Thalia is free software: you can redistribute it and/or modify
+ * it under the terms of the gnu general public license as published by
+ * the free software foundation, either version 3 of the license, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * merchantability or fitness for a particular purpose. see the
+ * gnu general public license for more details.
+ *
+ * You should have received a copy of the gnu general public license
+ * along with this program. if not, see <https://www.gnu.org/licenses/>.
+ *)
+
+val scan : (Token.t -> bool) -> string -> Token.t list
+val scan_all : string -> Token.t list
+
