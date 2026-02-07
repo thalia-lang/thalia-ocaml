4e1d12cad4ab8bfff34e8f587ab49ea6946e947e
Stan Vlad <vstan02@protonmail.com>
2026-02-07 20:16:19 +0200

    Recreated the automaton module.

---
+++ lib/automaton.ml
@@ -0,0 +1,43 @@
+(* Copyright (c) 2025 Stan Vlad <vstan02@protonmail.com>
+ *
+ * This file is part of Thalia.
+ *
+ * Thalia is free software: you can redistribute it and/or modify
+ * it under the terms of the gnu general public license as published by
+ * the free software foundation, either version 3 of the license, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * merchantability or fitness for a particular purpose. see the
+ * gnu general public license for more details.
+ *
+ * You should have received a copy of the gnu general public license
+ * along with this program. if not, see <https://www.gnu.org/licenses/>.
+ *)
+
+module type BASE = sig
+  type input
+end
+
+module type T = sig
+  include BASE
+  include Monad.T
+    with type 'a t = input -> input * 'a
+end
+
+module Make(M : BASE) = struct
+  type input = M.input
+
+  module Monad_base = struct
+    type 'a t = input -> input * 'a
+
+    let pure x = fun s -> s, x
+    let bind m f =
+      fun s ->
+        let s', x = m s in
+        f x s'
+  end
+  include Monad.Make(Monad_base)
+end
+
